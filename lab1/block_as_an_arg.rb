77f4e01054b101660a7abd7beab95abc0a7fb4a7
Corray333 <mark.corray.off@gmail.com>
2024-09-08 14:50:03 +0300

    feat(lab1): add useless code
    Lab1 task 4

--- lab1/block_as_an_arg.rb
+++ lab1/block_as_an_arg.rb
@@ -26,3 +26,26 @@ def count_elements_in_interval(arr, a, b)
   end
   return count
 end
+
+# Дан целочисленный массив. Необходимо найти количество элементов между первым и последним минимальным.
+
+def count_elements_between_min(arr)
+  return nil  if arr.empty?
+  min = arr[0]
+  min_index = 0
+  
+  arr.each_index do |i|
+    if arr[i] < min
+      min = arr[i]
+      min_index = i
+    end
+  end
+
+  first_min_index = min_index
+  last_min_index = min_index
+  
+  arr.each_index do |i|
+    first_min_index = i if arr[i] == min
+  end
+  return last_min_index - first_min_index
+end

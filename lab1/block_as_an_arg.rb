b847bad7e39a48d152cd181b32396dd92a718588
Corray333 <mark.corray.off@gmail.com>
2024-09-08 14:49:18 +0300

    add method to count elements in interval

--- lab1/block_as_an_arg.rb
+++ lab1/block_as_an_arg.rb
@@ -16,3 +16,13 @@ def find_min_index(arr)
   return min_index
 
 end
+
+# Дан целочисленный массив и интервал a..b. Необходимо найти количество элементов в этом интервале.
+
+def count_elements_in_interval(arr, a, b)
+  count = 0
+  arr.each_index do |i|
+    count += 1 if arr[i] > a and arr[i] < b
+  end
+  return count
+end

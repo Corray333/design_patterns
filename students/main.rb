8cedb953c9d33ed68b8c3f61ca101b8a5a569dd7
Corray333 <mark.corray.off@gmail.com>
2024-09-21 10:12:35 +0300

    refactor(lab2): format code

--- students/main.rb
+++ students/main.rb
@@ -1,29 +1,31 @@
 require "./entities"
 
-students = []
+def main()
+  students = []
 
-students << Student.new(
-  :id => 1,
-  surname: "Anikin",
-  name: "Mark",
-  patronymic: "Andreevich",
-  phone: "89320509129",
-  git: "github.com/Corray333"
-)
-students << Student.new({
-  id:2,
-  surname: "Masenko",
-  name: "Maria",
-  patronymic: "Sergeevna",
-  phone: "89234567890",
-})
+  students << Student.new(
+    :id => 1,
+    surname: "Anikin",
+    name: "Mark",
+    patronymic: "Andreevich",
+    phone: "89320509129",
+    git: "github.com/Corray333"
+  )
+  students << Student.new({
+    id:2,
+    surname: "Masenko",
+    name: "Maria",
+    patronymic: "Sergeevna",
+    phone: "89234567890",
+  })
 
-students[0].set_contacts({
-  tg_username: "@corray9",
-  email: "mark.corray.off@gmail.com"
-})
+  students[0].set_contacts({
+    tg_username: "@corray9",
+    email: "mark.corray.off@gmail.com"
+  })
 
-students.each do |student|
-  p(student)
+  students.each do |student|
+    p(student)
+  end
 end
 

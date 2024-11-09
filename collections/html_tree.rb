d6a722389faa0791e3f8c08fa3a8d5b5e97ee4cf
Corray333 <mark.corray.off@gmail.com>
2024-11-09 07:55:23 +0300

    feat(lab3, task3): remove tree class, split html tag to node + inherited htmlnode classes

--- collections/html_tree.rb
+++ collections/html_tree.rb
@@ -1,24 +1,46 @@
-require "./html_tag"
+# require "./html_tag"
 
-class HTMLTree
-  include Enumerable
+class Node
 
-  attr_accessor :root
+  attr_reader :children
 
-  def initialize(html)
-    @root = build_tree(html)
+  def initialize()
+    @children = []
   end
 
-  def build_tree(html)
+  def add_child(node)
+    @children << node
+  end
+
+  def each(&block)
+    traverse_depth(self, &block)
+  end
+
+  def traverse_depth(node, &block)
+    return if node.nil?
+
+    block.call(node)
+    node.children.each { |child| traverse_depth(child, &block) }
+  end
+end
+
+class HTMLNode < Node
+  def initialize(tag, attributes = {})
+    super()
+    @name = tag
+    @attributes = attributes
+  end
+
+  def to_s
+    "<#{@name} #{@attributes.inspect}>"
+  end
+
+  def self.build_tree(html)
     tokens = html.scan(/<[^>]+>|[^<]+/)
-    parse_node(tokens)
-  end
-
-  def parse_node(tokens)
     tag_stack = []
     current_tag = nil
     root_tag = nil
-
+  
     until tokens.empty?
       token = tokens.shift
       if token.start_with?('<')
@@ -27,28 +49,19 @@ class HTMLTree
         else
           tag_name = token.match(/<(\w+)/)[1]
           attributes = token.scan(/(\w+)='([^']+)/).to_h
-          new_tag = HTMLTag.new(tag_name, attributes)
+          new_tag = HTMLNode.new(tag_name, attributes)
           current_tag.add_child(new_tag) if current_tag
-          tag_stack.push(current_tag) if current_tag
-          current_tag = new_tag
-          root_tag ||= current_tag
+          root_tag ||= new_tag
+          unless token.end_with?('/>')
+            tag_stack.push(current_tag) if current_tag
+            current_tag = new_tag
+          end
         end
       else
-        current_tag.add_child(HTMLTag.new('text', { 'content' => token.strip })) if !token.strip.empty?
+        current_tag.add_child(HTMLNode.new('text', { 'content' => token.strip })) if !token.strip.empty?
       end
     end
     return root_tag
-  end
-
-  def each(&block)
-    traverse_depth(@root, &block)
-  end
-
-  def traverse_depth(node, &block)
-    return if node.nil?
-
-    block.call(node)
-    node.children.each { |child| traverse_depth(child, &block) }
   end
 end
 
